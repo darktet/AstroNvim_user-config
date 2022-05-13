b99a98ac687493f17dc0cc07b534048b98952ff9
darktet <santobollove@gmail.com>
2022-05-13 15:03:25 +0600

    clangd

--- lsp/server-settings/clangd.lua
+++ lsp/server-settings/clangd.lua
@@ -2,7 +2,7 @@ local util = require 'lspconfig.util'
 local root_pattern = util.root_pattern("compile_commands.json", "compile_flags.txt", ".git", "CMakeLists.txt", "src");
 
 return {
-  cmd = { '/home/vash/.local/share/nvim/lsp_servers/clangd/clangd/bin/clangd', '--log=error', '--background-index', '--fallback-style=gnu', '--malloc-trim', '--all-scopes-completion', '--ranking-model=decision_forest' },
+  cmd = { 'clangd', '--clang-tidy',  '--log=error', '--background-index', '--fallback-style=mozilla', '--malloc-trim', '--all-scopes-completion', '--ranking-model=decision_forest' },
   filetypes = { 'c', 'cpp', 'objc', 'objcpp' },
   single_file_support = true,
   root_dir = function(fname)
