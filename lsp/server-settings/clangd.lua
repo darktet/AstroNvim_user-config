15b7278a2b47c5eddd2c9b77a4c516060411c343
darktet <santobollove@gmail.com>
2022-05-19 12:55:47 +0600

    Test

--- lsp/server-settings/clangd.lua
+++ lsp/server-settings/clangd.lua
@@ -2,7 +2,7 @@ local util = require 'lspconfig.util'
 local root_pattern = util.root_pattern("compile_commands.json", "compile_flags.txt", ".git", "CMakeLists.txt", "src");
 
 return {
-  cmd = { 'clangd', '--clang-tidy',  '--log=error', '--background-index', '--fallback-style=mozilla', '--malloc-trim', '--all-scopes-completion', '--ranking-model=decision_forest' },
+  cmd = { 'clangd', '--enable-config', '--log=error', '--background-index', '--fallback-style=mozilla', '--malloc-trim', '--all-scopes-completion', '--ranking-model=decision_forest' },
   filetypes = { 'c', 'cpp', 'objc', 'objcpp' },
   single_file_support = true,
   root_dir = function(fname)
